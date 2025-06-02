321f84bd4da9c9c1e5778188fcf4d6c03798d389
Long Huynh 2 <long.huynh@siliconprime.com>
2025-06-02 09:11:55 +0700

    Refactor Banner component to use Ant Design's Image component for better performance and consistency

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Carousel } from 'antd';
+import { Carousel, Image } from 'antd';
 import './Banner.css';
 
 const Banner = (...props) => {
   return (
     <Carousel autoplay dots={true} dotPosition='bottom' className='banner-carousel' {...props}>
       <div className="carousel-item">
-        <img src="image1.jpg" alt="Image 1" className="square-image" />
+        <Image src={'image1.png'} preview={false}/>
       </div>
       <div className="carousel-item">
-        <img src="image2.jpg" alt="Image 2" className="square-image" />
+        <Image src={'image2.png'} preview={false}/>
       </div>
       <div className="carousel-item">
-        <img src="image3.jpg" alt="Image 3" className="square-image" />
+        <Image src={'image3.png'} preview={false}/>
       </div>
       <div className="carousel-item">
-        <img src="image4.jpg" alt="Image 4" className="square-image" />
+        <Image src={'image4.png'} preview={false}/>
       </div>
     </Carousel>
   );
 };
 
-export default Banner; 
+export default Banner;
