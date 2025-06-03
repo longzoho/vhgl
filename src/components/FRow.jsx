43eb4aa74e8ead984eca67a3e2bba9f5c6a65ab1
Long Huynh 2 <long.huynh@siliconprime.com>
2025-06-03 10:40:46 +0700

    Comment out IntersectionObserver logic in FRow component

--- src/components/FRow.jsx
+++ src/components/FRow.jsx
@@ -4,28 +4,30 @@ import { Row } from 'antd';
 const FRow = (props) => {
   const rowRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          rowRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-      },
-      { threshold: 0.5 }
-    );
-
-    const currentRowRef = rowRef.current;
-
-    if (currentRowRef) {
-      observer.observe(currentRowRef);
-    }
-
-    return () => {
-      if (currentRowRef) {
-        observer.unobserve(currentRowRef);
-      }
-    };
-  }, []);
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       if (entry.isIntersecting) {
+  //         rowRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  //       } else {
+  //         console.log('Element is not intersecting, scrollIntoView will not be applied.');
+  //       }
+  //     },
+  //     { threshold: 0.5 }
+  //   );
+  //
+  //   const currentRowRef = rowRef.current;
+  //
+  //   if (currentRowRef) {
+  //     observer.observe(currentRowRef);
+  //   }
+  //
+  //   return () => {
+  //     if (currentRowRef) {
+  //       observer.unobserve(currentRowRef);
+  //     }
+  //   };
+  // }, []);
   return <Row ref={rowRef}  {...props} style={{ ...(props.style || {}), minHeight: '100vh' }}/>;
 }
 
